a45cd2a42a2d29d727fdc21c387ab01a882cd294
frostyeahh <andresz11alvarez@gmail.com>
2024-01-20 19:10:37 -0800

    new

---
+++ src/components/pages/website/php/insert.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+ 
+<head>
+    <title>Data Portfolio</title>
+</head>
+ 
+<body>
+    <center>
+        <?php
+ 
+        // servername => localhost
+        // username => root
+        // password => empty
+        // database name => staff
+        $conn = mysqli_connect("localhost", "Frost", "eLD8Vs7*8qr,-g#mmPZo", "admin");
+         
+        // Check connection
+        if($conn === false){
+            die("ERROR: Could not connect. "
+                . mysqli_connect_error());
+        }
+         
+        // Taking all 5 values from the form data(input)
+        $first_name =  $_REQUEST['first_name'];
+        $email = $_REQUEST['email'];
+        $message = $_REQUEST['message']
+         
+        // Performing insert query execution
+        // here our table name is college
+        $sql = "INSERT INTO ADMIN  VALUES ('$first_name', 
+            ,'$email', '$message')";
+         
+        if(mysqli_query($conn, $sql)){
+            echo "<h3>data stored in a database successfully."
+                . " Please browse your localhost php my admin"
+                . " to view the updated data</h3>"; 
+ 
+            echo nl2br("\n$first_name\n $email\n "
+        . "$message\n");
+        } else{
+            echo "ERROR: Hush! Sorry $sql. "
+                . mysqli_error($conn);
+        }
+         
+        // Close connection
+        mysqli_close($conn);
+        ?>
+    </center>
+</body>
+ 
+</html>
