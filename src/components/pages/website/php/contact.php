31508d6e20f90f602a877c15f2e67766156a8f1d
frostyeahh <andresz11alvarez@gmail.com>
2024-01-21 16:34:03 -0800

    fixed again

--- src/components/pages/website/php/contact.php
+++ src/components/pages/website/php/contact.php
@@ -1,22 +1,32 @@
 <?php
-// database connection code
-// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
 
-$con = mysqli_connect('localhost', 'root', '','db_contact');
-
-// get the post records
-$txtName = $_POST['txtName'];
-$txtEmail = $_POST['txtEmail'];
-$txtPhone = $_POST['txtPhone'];
-$txtMessage = $_POST['txtMessage'];
-
-// database insert SQL code
-$sql = "INSERT INTO `tbl_contact` (`Id`, `fldName`, `fldEmail`, `fldPhone`, `fldMessage`) VALUES ('0', '$txtName', '$txtEmail', '$txtPhone', '$txtMessage')";
-
-// insert in database 
-$rs = mysqli_query($con, $sql);
-
-if($rs)
+if(isset($_POST['submit']))
 {
-	echo "Contact Records Inserted";
+    $txtName = $_POST['txtName'];
+    $txtEmail = $_POST['txtEmail'];
+    $txtPhone = $_POST['txtPhone'];
+    $txtMessage = $_POST['txtMessage'];
 }
+
+$host = "localhost";
+$username = "root";
+$password - "";
+$dbname = "db_contact";
+
+$con = mysqli_connect($host, $username, $password, $dbname);
+
+if (!$con)
+{
+    die("Connection Failed!" . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO contactform_entries (txtName, txtEmail, txtPhone, txtMessage) VALUES ('0', '$txtName', '$txtEmail', '$txtPhone', '$txtMessage')";
+
+$rs = mysqli_query($con, $sql);
+if ($rs)
+{
+    echo "Entries Added!";
+}
+
+mysqli_close($con);
+
